bba9617d86724a6c707ea45a5a295aabd9019e5a
Bud Welsh <budwelsh14@gmail.com>
2018-12-25 18:51:24 -0600

    Add comments

---
+++ stopwatch.js
@@ -0,0 +1,7 @@
+/*
+    Title:          Hot Lap Stopwatch
+    Date:           12/25/2018
+    Author:         Bud Welsh
+    Description:    A stopwatch that will show lap times and lap average speeds
+*/
+
