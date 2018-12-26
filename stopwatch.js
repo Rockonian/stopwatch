ae05b137c74f86362a4b5c78c8147a44b725dffa
Bud Welsh <budwelsh14@gmail.com>
2018-12-25 20:27:19 -0600

    Add zeros and colons to console.log()

--- stopwatch.js
+++ stopwatch.js
@@ -9,19 +9,32 @@
 var startTime = Date.now(); // Stopwatch starts with the current time in milliseconds
 var stopTime = startTime + 12666; // ******* Stop time testing this will change ********
 var lapTime = stopTime - startTime; // Lap time in milliseconds
+// Used in if statement
+var lapMS = 000;
+var lapSec = 00; 
+var lapMin = 00;
+var lapHr = 00;
 
 // Checking the variables through console.log()
 console.log("Starting time in milliseconds: " + startTime);
 console.log("Stopping time in milliseconds: " + stopTime);
 console.log("Lap time in milliseconds: " + lapTime);
 
-// Fallthrough if statements to separate the times into hours, minutes, seconds and milliseconds
+// Fall through if statements to separate the times into hours, minutes, seconds and milliseconds
 if (lapTime >= 3600000) {
-    console.log("The lap time is hours long");    
+    console.log("The lap time is hours long"); 
+    lapSec = lapTime / 1000;
+    console.log(lapHr + ":" + lapMin + ":" + lapSec);
 }else if (lapTime >= 60000) {
     console.log("The lap time is minutes long");
+    lapSec = lapTime / 1000;
+    console.log(lapHr + ":" + lapMin + ":" + lapSec);
 }else if (lapTime >= 1000) {
     console.log("The lap time is seconds long");
+    lapSec = lapTime / 1000;
+    console.log(lapHr + ":" + lapMin + ":" + lapSec);
 }else {
     console.log("The lap time is less than a second");
+    lapSec = lapTime / 1000;
+    console.log(lapHr + ":" + lapMin + ":" + lapSec);
 }
