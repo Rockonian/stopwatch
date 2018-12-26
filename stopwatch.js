d7bef9ccf87f348ad85154e32510336aa7ecff98
Bud Welsh <budwelsh14@gmail.com>
2018-12-25 19:00:23 -0600

    Get start time in milliseconds

--- stopwatch.js
+++ stopwatch.js
@@ -5,3 +5,9 @@
     Description:    A stopwatch that will show lap times and lap average speeds
 */
 
+// Declaring and initializing variables
+var startTime = Date.now(); // Stopwatch starts with the current time in milliseconds
+
+
+// Checking the variables through console.log()
+console.log("Starting time in milliseconds: " + startTime);
