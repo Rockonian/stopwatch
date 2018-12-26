596e81051f5b78fefd5bd5f9c6a9ee3ac3913464
Bud Welsh <budwelsh14@gmail.com>
2018-12-25 19:10:32 -0600

    Add math that gets lap time

--- stopwatch.js
+++ stopwatch.js
@@ -8,9 +8,10 @@
 // Declaring and initializing variables
 var startTime = Date.now(); // Stopwatch starts with the current time in milliseconds
 var stopTime = startTime + 12666; // ******* Stop time testing this will change ********
+var lapTime = stopTime - startTime; // Lap time in milliseconds
 
 // Checking the variables through console.log()
 console.log("Starting time in milliseconds: " + startTime);
 console.log("Stopping time in milliseconds: " + stopTime);
-
-
+console.log("Lap time in milliseconds: " + lapTime);
+ 
